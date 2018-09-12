3193545a14105b23170e7cceeb6107c131c78332
Rhenan Bartels <rhenan.bartels@gmail.com>
2018-09-12 16:43:33 -0300

    Retorna lista orgaos

--- orgaos/models.py
+++ orgaos/models.py
@@ -1,3 +1,40 @@
-from django.db import models
+import cx_Oracle
 
-# Create your models here.
+from decouple import config
+
+
+list_orgaos_query = """
+    SELECT
+        org.ORLW_ORGI_CDORGAO AS CDORG,
+        org.ORLW_REGI_NM_REGIAO AS CRAAI,
+        org.ORLW_CMRC_NM_COMARCA AS COMARCA,
+        org.ORLW_COFO_NM_FORO AS FORO,
+        org.ORLW_ORGI_NM_ORGAO AS ORGAO,
+        func.NMFUNCIONARIO AS TITULAR
+    FROM
+        ORGI_VW_ORGAO_LOCAL_ATUAL org
+        LEFT JOIN MPRJ_VW_FUNCIONARIO func ON func.CDORGAO = org.ORLW_DK
+    WHERE
+        1=1
+        AND org.ORLW_TPOR_DK = 1
+        AND (org.ORLW_ORGI_DT_FIM IS NULL
+            OR org.ORLW_ORGI_DT_FIM > SYSDATE)
+        AND func.CDTIPFUNC = 1
+        AND func.CDSITUACAOFUNC = 1
+        AND func.CDCARGO = 74
+"""
+
+
+def run(query, params):
+    connection = cx_Oracle.connect(
+            user=config('DS_EXADATA_user'),
+            password=config('DS_EXADATA_password'),
+            dsn=config('DS_EXADATA_CONN_SID'),
+            encoding="UTF-8",
+            nencoding="UTF-8"
+    )
+
+    cursor = connection.cursor()
+    if params is None:
+        return cursor.execute(query)
+    return cursor.execute(query, params)
--- orgaos/views.py
+++ orgaos/views.py
@@ -1,7 +1,22 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .model import run, list_orgaos_query
+
 
 class OrgaosView(APIView):
     def get(self, request, *args, **kwargs):
-        return Response(data={})
+        data = run(list_orgaos_query)
+        results = []
+        for row in data:
+            row_dict = {
+                'CDORG': row[0],
+                'CRAAI': row[1],
+                'COMARCA': row[2],
+                'FORO': row[3],
+                'ORGAO': row[4],
+                'TITULAR': row[5],
+            }
+            results.append(row_dict)
+
+        return Response(data=results)
