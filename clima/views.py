c950891633b3329745f1f3ad56f22f775daf17bc
Rhenan Bartels <rhenan.bartels@gmail.com>
2018-09-12 16:26:44 -0300

    Inicia uso de djago rest framework

--- clima/views.py
+++ clima/views.py
@@ -1,15 +1,15 @@
 import requests
 
-from django.http import JsonResponse
-from django.views.generic import View
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from metar import Metar
 
 URL = "http://tgftp.nws.noaa.gov/data/observations/metar/stations/SBRJ.TXT"
 
 
-class ClimaView(View):
+class ClimaView(APIView):
     def get(self, request, *args, **kwargs):
         resp_content = requests.get(URL).content.decode('UTF-8')
         metar_obj = Metar.Metar('METAR ' + resp_content.split('\n')[1])
         temperature = str(metar_obj.temp).split(' ')[0]
-        return JsonResponse(data={'temperature': temperature})
+        return Response(data={'temperature': temperature})
